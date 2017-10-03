853de7371fca60fa7214227cf13e507d6294cb58
vthost <veronika.thost@gmail.com>
2017-10-03 17:33:49 +0200

    reorg; query also for results

---
+++ src/sparql/synthesis/data/Config.java
@@ -0,0 +1,13 @@
+package sparql.synthesis.data;
+
+public class Config {
+	
+	public static String DATA_DIR = "data/";
+	
+	public static String QUERY_FILE_EXT = ".txt";
+	
+	public static String QUERY_DATA_FILE_EXT = "-data.txt";
+	
+	public static String QUERY_RESULT_FILE_EXT = "-result.txt";
+	
+}
