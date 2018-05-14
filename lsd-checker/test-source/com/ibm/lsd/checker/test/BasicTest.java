55aea5d1c8e41ac2be3cb17721d8b231ad73597a
Julian Dolby <juliandolby@gmail.com>
2018-05-13 21:29:23 -0400

    add simple expansion test

--- lsd-checker/test-source/com/ibm/lsd/checker/test/BasicTest.java
+++ lsd-checker/test-source/com/ibm/lsd/checker/test/BasicTest.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import org.junit.Test;
 
 import com.ibm.lsd.checker.drivers.LSDCheckerDriver;
+import com.ibm.lsd.checker.drivers.LSDExpandEachDriver;
 
 public class BasicTest {
 
@@ -18,7 +19,20 @@ public class BasicTest {
 	}
 
 	@Test
-	public void test626806() throws URISyntaxException, IOException {
+	public void testCheck626806() throws URISyntaxException, IOException {
 		testQuery("DBpedia-q626806.rq");
 	}
+	
+	public void testExpand(String query) throws Exception {
+		URL url = BasicTest.class.getClassLoader().getResource(query);
+		assert url != null;
+		
+		LSDExpandEachDriver.main(new String[] {url.toString()});
+	}
+
+	@Test
+	public void testExpand626806() throws Exception {
+		testExpand("DBpedia-q626806.rq");
+	}
+
 }
